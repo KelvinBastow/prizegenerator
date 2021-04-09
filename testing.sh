bbb2e1f065c6113d076a02c8db06bd16140799ed
kelvin bastow <kelvinb97@outlook.com>
2021-04-09 15:31:45 +0000

    Update 09/04/2021 - 16:31

--- testing.sh
+++ testing.sh
@@ -3,5 +3,6 @@ sudo apt-get install python3-venv
 python3 -m venv venv
 source venv/bin/activate
 pip3 install - r requirements.txt
+pip3 install pytest
 
 python3 -m pytest --cov=, --cov-report=missing-term
