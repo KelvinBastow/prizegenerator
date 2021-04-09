38aa2c5173f929afe2332592c93c7454ce1f09e6
kelvin bastow <kelvinb97@outlook.com>
2021-04-09 15:37:16 +0000

    Update 09/04/2021 - 16:33

--- testing.sh
+++ testing.sh
@@ -6,4 +6,7 @@ pip3 install - r requirements.txt
 pip3 install pytest
 pip3 install pytest-cov
 
-python3 -m pytest --cov=, --cov-report=missing-term
+python3 -m pytest Service1 --cov=Service1 --cov-report=term-missing
+python3 -m pytest Service2 --cov=Service2 --cov-report=term-missing
+python3 -m pytest Service3 --cov=Service3 --cov-report=term-missing
+python3 -m pytest Service4 --cov=Service4 --cov-report=term-missing
