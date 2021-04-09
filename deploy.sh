955c1b5b8898a9553ef89e01b507ed6774b5a94b
kelvin bastow <kelvinb97@outlook.com>
2021-04-09 17:18:03 +0000

    Update 09/04/2021 - 18:18

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-scp -i ~/.ssh/ansible_id_rsa docker-compose.yaml jenkins@deathstar-master:/home/jenkins/docker-compose.yaml
-ssh -i ~/.ssh/ansible_id_rsa jenkins@deathstar-master << EOF
+scp -i ~/.ssh/id_rsa docker-compose.yaml jenkins@deathstar-master:/home/jenkins/docker-compose.yaml
+ssh -i ~/.ssh/id_rsa jenkins@deathstar-master << EOF
     export DATABASE_URI=${DATABASE_URI}
     docker stack deploy --compose-file /home/jenkins/docker-compose.yaml prizegenerator
 EOF
