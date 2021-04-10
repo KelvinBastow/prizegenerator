a0f75261bb60281982198f5bdb5cfdab472d06b0
kelvin bastow <kelvinb97@outlook.com>
2021-04-10 12:37:09 +0000

    Update 10/04/2021 - 13:37

--- service1/app.py
+++ service1/app.py
@@ -1,20 +1,29 @@
 from os import error
+from prizegenerator.service2.app import random_number_generator
 from flask import Flask, render_template, Response, request
-# from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 import requests
-# from os import getenv
+from os import getenv
 
 app = Flask(__name__)
 
-# app.config['SQLALCHEMY_DATABASE_URI'] = getenv("DATABASE_URI")
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS] = False
-# db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = getenv("DATABASE_URI")
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS] = False
+db = SQLAlchemy(app)
+
+class PrizeGenerator(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    random_number = db.Column(db.String(50), nullable = False)
+    random_letter = db.Column(db.String(50), nullable = False)
 
 @app.route("/", methods=['GET'])
 @app.route("/prizegenerator", methods=['GET'])
 def home():
     service_four_default_response=requests.get("http://service4:5003/prizecreator").json()
     prize = f'{service_four_default_response["prize_money"]}'
+    randomprize = models.PrizeGenerator(random_number = service4['random_number'], random_letter = service4['random_letter'])
+    db.session.add(randomprize)
+    db.session.commit()
 
     return render_template('homepage.html', message=prize)
 
