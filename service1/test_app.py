3c0d38e9b7464e7e5bea838e5f9ac74bb15eef2f
kelvin bastow <kelvinb97@outlook.com>
2021-04-10 15:36:16 +0000

    Update 10/04/2021 - 16:36

--- service1/test_app.py
+++ service1/test_app.py
@@ -1,7 +1,6 @@
-from prizegenerator.service1.app import PrizeGenerator
+from app import app, db, PrizeGenerator
 from flask_testing import TestCase
 from flask import url_for
-from app import app ,db
 
 class TestBase(TestCase):
     def create_app(self):
