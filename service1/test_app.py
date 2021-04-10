de563e664c5bb3e8bf4c1882a2cd22c97bc61726
kelvin bastow <kelvinb97@outlook.com>
2021-04-10 15:13:27 +0000

    Update 10/04/2021 - 16:13

--- service1/test_app.py
+++ service1/test_app.py
@@ -5,22 +5,23 @@ from app import app ,db
 
 class TestBase(TestCase):
     def create_app(self):
-        app.config.update(SQLALCHEMY_DATABASE_URI="sqlite:///",
-                SECRET_KEY='TEST_SECRET_KEY',
-                DEBUG=True,
-                WTF_CSRF_ENABLED=False
-                )
         return app
+    #     app.config.update(SQLALCHEMY_DATABASE_URI="sqlite:///",
+    #             SECRET_KEY='TEST_SECRET_KEY',
+    #             DEBUG=True,
+    #             WTF_CSRF_ENABLED=False
+    #             )
+    #     return app
 
-    def setUp(self):
-        db.create_all()
-        prizegenerator = PrizeGenerator(random_number = form.random_number.data ,random_letter = form.random_letter.data)
-        db.session.add(prizegenerator)
-        db.session.commit()
+    # def setUp(self):
+    #     db.create_all()
+    #     prizegenerator = PrizeGenerator(random_number = form.random_number.data ,random_letter = form.random_letter.data)
+    #     db.session.add(prizegenerator)
+    #     db.session.commit()
 
-    def tearDown(self):
-        db.session.remove()
-        db.drop_all()
+    # def tearDown(self):
+    #     db.session.remove()
+    #     db.drop_all()
 
 class TestPrizeGenerator(TestBase):
     def test_prize_generator(self):
